d762dbc1cf8be34c5a104fc772ff2e97e2602b7d
Carlos Alpírez <carlos.alpi21@gmail.com>
2023-12-01 18:13:27 -0600

    [master] Added view pager 2 lib, element added to main activity xml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,8 @@ dependencies {
     // Firebase
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
     implementation("com.google.firebase:firebase-firestore-ktx")
+    // ViewPager2
+    implementation("androidx.viewpager2:viewpager2:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
