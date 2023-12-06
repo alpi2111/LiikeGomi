4d5e557847282955b9fa02bbe14d0401d7112551
Carlos Alpírez <carlos.alpi21@gmail.com>
2023-12-06 06:14:39 -0600

    [master] Added 'Carrito' model class for getting the data from firebase

---
+++ app/src/main/java/com/liike/liikegomi/background/firebase_db/entities/Carrito.kt
@@ -0,0 +1,29 @@
+package com.liike.liikegomi.background.firebase_db.entities
+
+
+import com.google.firebase.firestore.PropertyName
+
+class Carrito(
+    @get:PropertyName("nombre_usuario")
+    @set:PropertyName("nombre_usuario")
+    var nombreUsuario: String?,
+    @get:PropertyName("productos")
+    @set:PropertyName("productos")
+    var productos: List<Producto?>?,
+) {
+    class Producto(
+        @get:PropertyName("cantidad")
+        @set:PropertyName("cantidad")
+        var cantidad: Int?,
+        @get:PropertyName("id_producto")
+        @set:PropertyName("id_producto")
+        var idProducto: Int?,
+        @get:PropertyName("precio_unidad")
+        @set:PropertyName("precio_unidad")
+        var precioUnidad: Double?,
+    ) {
+        constructor() : this(null, null, null)
+    }
+
+    constructor() : this(null, null)
+}
